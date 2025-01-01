0a4d90e9a1f2014afbfd08410f4d70c876c98e94
RJRYT <robinjratr@gmail.com>
2025-01-01 14:32:22 +0530

    removed role based authentication

--- server/src/routes/employeeRouter.js
+++ server/src/routes/employeeRouter.js
@@ -1,35 +1,28 @@
 const express = require("express");
 
-const { verifyJwt, authorize } = require("../middlewares/jwtAuth");
+const { verifyJwt } = require("../middlewares/jwtAuth");
 const {
-	getJobs,
-	getJob,
-	bookmarkJob,
-	getBookmarkedJobs,
-	deleteBookmarkedJob,
-	applyJob,
-    getAppliedJobs,
-	deleteAppliedJob,
+  getJobs,
+  getJob,
+  bookmarkJob,
+  getBookmarkedJobs,
+  deleteBookmarkedJob,
+  applyJob,
+  getAppliedJobs,
+  deleteAppliedJob,
 } = require("../controllers/employeeController");
 
 const router = express.Router();
 
-router.route("/jobs").get(verifyJwt, authorize("employee"), getJobs); // get all jobs by filter conditions
-router.route("/job/:id").get(verifyJwt, authorize("employee"), getJob); // get single job by id
-router
-	.route("/bookmark-job/:id")
-	.post(verifyJwt, authorize("employee"), bookmarkJob); // bookmark a job by id
-router
-	.route("/bookmark-job/:id")
-	.delete(verifyJwt, authorize("employee"), deleteBookmarkedJob); // delete bookmarded job
-router
-	.route("/bookmarked-jobs")
-	.get(verifyJwt, authorize("employee"), getBookmarkedJobs); // get all bookmarked jobs for user
-router.route("/apply-job/:id").post(verifyJwt, authorize("employee"), applyJob); // apply a job
+router.route("/jobs").get(verifyJwt, getJobs); // get all jobs by filter conditions
+router.route("/job/:id").get(verifyJwt, getJob); // get single job by id
+router.route("/bookmark-job/:id").post(verifyJwt, bookmarkJob); // bookmark a job by id
+router.route("/bookmark-job/:id").delete(verifyJwt, deleteBookmarkedJob); // delete bookmarded job
+router.route("/bookmarked-jobs").get(verifyJwt, getBookmarkedJobs); // get all bookmarked jobs for user
+router.route("/apply-job/:id").post(verifyJwt, applyJob); // apply a job
 
-router.route("/delete-job/:id").delete(verifyJwt, authorize("employee"), deleteAppliedJob); // delete applied job
+router.route("/delete-job/:id").delete(verifyJwt, deleteAppliedJob); // delete applied job
 
-router
-	.route("/applied-jobs")
-	.get(verifyJwt, authorize("employee"), getAppliedJobs); // get all applied jobs for user
+router.route("/applied-jobs").get(verifyJwt, getAppliedJobs); // get all applied jobs for user
+
 module.exports = router;
